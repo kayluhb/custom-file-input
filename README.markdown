b6e29138f48985d3ce7fa40a218935bfac77f6ab
kayluhb <cbrown@blenderbox.com>
2012-02-25 20:06:06 -0500

    tweaked the plugin and updated the README

--- README.markdown
+++ README.markdown
@@ -1,1 +1,14 @@
 # Custom Fileupload
+
+So this is a little script to fully customize the file input element w/ jQuery.
+
+Include the JS file and the CSS file in your app, and then call  
+
+` $('jquery-selector').customFile({ status:true, text: 'Choose File', root:'p' });  
+
+` status - a boolean of whether or not to show status on select  
+` text - the text to display  
+` root - the root element to replace the input with  
+
+The code is pretty easy to follow.
+
--- jquery.custom-file.js
+++ jquery.custom-file.js
@@ -1,6 +1,6 @@
 /* 
 
-Call the plugin with $('jquery-selector').customFile({ status:true, text: 'Choose File' });
+Call the plugin with $('jquery-selector').customFile({ status:true, text: 'Choose File', root:'p' });
 
 */
 
@@ -10,7 +10,7 @@ Call the plugin with $('jquery-selector').customFile({ status:true, text: 'Choos
         var settings = $.extend({}, $.fn.customFile.defaults, opts),
         $el = $(el), klass = 'custom-file', hover = '-hover', focus = '-focus',
         //create custom control container
-        upload = $('<p class="' + klass + ' ' + $el.attr('class') + '"></p>'),
+        upload = $('<' + settings.root + ' class="' + klass + ' ' + $el.attr('class') + '"></' + settings.root + '>'),
         //create custom control button
         button = $('<span class="' + klass + '-button" aria-hidden="true">' + settings.text + '</span>').appendTo(upload),
         //create custom control feedback
@@ -23,48 +23,48 @@ Call the plugin with $('jquery-selector').customFile({ status:true, text: 'Choos
             .mouseover(function(){ upload.addClass(klass + hover); })
             .mouseout(function(){ upload.removeClass(klass + hover); })
             .focus(function(){
-	              upload.addClass(klass + focus); 
-	              $el.data('val', $el.val());
+                  upload.addClass(klass + focus); 
+                  $el.data('val', $el.val());
             })
             .blur(function(){ 
-	              upload.removeClass(klass + focus);
-	              $(this).trigger('checkChange');
+                  upload.removeClass(klass + focus);
+                  $(this).trigger('checkChange');
              })
             .bind('checkChange', function(){
-	              if ($el.val() && $el.val() != $el.data('val')){
-		                $el.trigger('change');
-	              }
+                  if ($el.val() && $el.val() != $el.data('val')){
+                        $el.trigger('change');
+                  }
             })
             .bind('change',function(){
                 // Return if there' no status to update
                 if (!settings.status) { return; }
-	              // Get file name
-	              var fileName = $(this).val().split(/\\/).pop(),
-	              // Get file extension
-	              fileExt = klass + '-ext-' + fileName.split('.').pop().toLowerCase();
-	              // Update the feedback
-	              feedback
-		              .text(fileName) // Set feedback text to filename
-		              .removeClass(feedback.data('fileExt') || '') // Remove any existing file extension class
-		              .addClass(fileExt) // Add file extension class
-		              .data('fileExt', fileExt) // Store file extension for class removal on next change
-		              .addClass(klass + '-feedback-populated'); // Add class to show populated state
+                  // Get file name
+                  var fileName = $(this).val().split(/\\/).pop(),
+                  // Get file extension
+                  fileExt = klass + '-ext-' + fileName.split('.').pop().toLowerCase();
+                  // Update the feedback
+                  feedback
+                      .text(fileName) // Set feedback text to filename
+                      .removeClass(feedback.data('fileExt') || '') // Remove any existing file extension class
+                      .addClass(fileExt) // Add file extension class
+                      .data('fileExt', fileExt) // Store file extension for class removal on next change
+                      .addClass(klass + '-feedback-populated'); // Add class to show populated state
             })
             .click(function(){ //for IE and Opera, make sure change fires after choosing a file, using an async callback
-	              $el.data('val', $el.val());
-	              setTimeout(function(){
-		                $el.trigger('checkChange');
-	              },100);
+                  $el.data('val', $el.val());
+                  setTimeout(function(){
+                        $el.trigger('checkChange');
+                  },100);
             });
         // On mousemove, keep file input under the cursor
         upload
-	          .mousemove(function(e){
-		            $el.css({
-			            'left': e.pageX - upload.offset().left - $el.outerWidth() + 20, // Position right side 20px right of cursor X)
-			            'top': e.pageY - upload.offset().top - 5
-		            });	
-	          })
-	          .insertAfter($el); // Insert after the input
+              .mousemove(function(e){
+                    $el.css({
+                        'left': e.pageX - upload.offset().left - $el.outerWidth() + 20, // Position right side 20px right of cursor X)
+                        'top': e.pageY - upload.offset().top - 5
+                    });
+              })
+              .insertAfter($el); // Insert after the input
         $el.appendTo(upload).css({ opacity:0 });
     };
     $.fn.customFile = function(options) {
@@ -79,6 +79,5 @@ Call the plugin with $('jquery-selector').customFile({ status:true, text: 'Choos
         });
     };
     // default settings
-    $.fn.customFile.defaults = { status:true, text: 'Choose File' };
+    $.fn.customFile.defaults = { status: true, text: 'Choose File', root: 'p' };
 })(jQuery);
-
